c31c4a68ecaf380680e0b8d87506ea901acb20f8
CGdiego <266080606+CGdiego@users.noreply.github.com>
2026-08-13 10:58:32 -0300

    Updated the code

--- Modulo01/PBL01.c
+++ Modulo01/PBL01.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World");
+    char c1, c2, c3;
+    int N1, N2;
+    float Q;
+
+    printf("Insira três inteiros, e pressione ENTER a cada um: \n");
+    scanf(" %c", &c1);
+    scanf(" %c", &c2);
+    scanf(" %c", &c3);
+
+    N1 = (c1 - '0') * 100 + (c2 - '0') * 10 + (c3 - '0');
+
+    printf("\nInsira três inteiros, e pressione ENTER a cada um: \n");
+    scanf(" %c", &c1);
+    scanf(" %c", &c2);
+    scanf(" %c", &c3);
+
+    N2 = (c1 - '0') * 100 + (c2 - '0') * 10 + (c3 - '0');
+
+    Q = (float) N1 / N2;
+    printf("\n%8.3f", Q);
+
     return 0;
 }
